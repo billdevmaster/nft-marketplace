ed83aed8c1d36a7bde13b401ce43fba0284fc126
webdevsmart <webdevsmart@hotmail.com>
2021-08-30 22:20:37 +1000

    init commit

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,31 @@
+export const TokenAddress = '0x4AD910956D7E08cC9b2BB0e991c9998ee86DDB8d';
+export const FactoryAddress = '0x7c26BF1f1A6D07dA9781AB001B00E7037Fe08371';
+export const AuctionAddress = '0x0287e178610C9259AB5F932F613622cA360454f8';
+export const AuctionBNBAddress = '0x0A269272686D566Ca55D0BF88aBf0f3D4422f730';
+
+export const NFTStorageKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDUxODlEMzhCRDVmMWE5MmU0RjIzNUVmMDExRTMzMjRiNUZDRkNjZTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDgwNDI1MjI0OSwibmFtZSI6Im1vb25zdGFyIn0.ioVYvZ_sYN-a89eGkMYJNPK-kRVx9vt0CdxGfQmtqg0';
+export const DefaultChainID = 3;
+export const ChainList = {
+  1: 'Mainnet',
+  3: 'Ropsten',
+  4: 'RinkeBy',
+  5: 'Goerli',
+  56: 'BSC mainnet',
+  97: 'BSC Testnet',
+};
+
+export const DefaultAvatar =
+  'https://ipfs.io/ipfs/QmRRXBjQRQBdnivJuGe9bgk4yqogULSYoLmcy32y6AierY';
+
+export const CoinGeckoAPIForBNB =
+  'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd';
+
+export const CoinGeckoAPIForMoonstar =
+  'https://api.coingecko.com/api/v3/simple/price?ids=MOONSTAR&vs_currencies=usd';
+//
+// OLD MARKETPLACE ADDRESSES
+// 0x06B45A2DA9EdbA591a2F28e4CB3f094f9b07D62B
+
+// 0xc3C0Fe963c89e84A9cE21AB76FC69CF22C1b077b
+//0xff3efbff3ef7a6bcc7a7b533f709bcc0e2e6cd30
