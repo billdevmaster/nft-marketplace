9da428976af2e20fb1d3b027d6fc1c1a3ad2acc6
webdevsmart <webdevsmart@hotmail.com>
2021-08-30 23:57:43 +1000

    commit for progress

--- backend/controller/apiCtrl.js
+++ backend/controller/apiCtrl.js
@@ -13,7 +13,6 @@ const saveCollection = (req, res) => {
 }
 
 const collections = async ( req, res ) => {
-    console.log("test");return;
     const address = req.body.account;
     let query = {};
     if (address != undefined) {
